3833b2fe716b2ae9e67766f21f979ecc5fbba8c7
SwapnithKumar <swapnithmadire123@gmail.com>
2024-03-23 10:23:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Component } from 'react';
+import MainContainer from './MainContainer'
+import About from './About';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {isDark:false}
+
+  changeTheme = ()=>{
+    this.setState(prevState=>({isDark:!prevState.isDark}))
+  }
+
+  render(){
+    const {isDark} = this.state
+    const theme = isDark === true ? 'light-text' : 'dark-text';
+    return(
+       <div className={`${theme}`}>
+        <BrowserRouter>
+           <Routes>
+            <Route path = '/' element={<MainContainer changeTheme = {this.changeTheme} isDark = {isDark} />}/>
+            <Route path = '/about' element = {<About isDark = {isDark} changeTheme = {this.changeTheme}/>}/>
+           </Routes>
+        </BrowserRouter>
+       </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/MainContainer/index.js
@@ -0,0 +1,38 @@
+import { Component } from 'react'
+import './index.css'
+import Header from '../Header'
+import Footer from '../Footer'
+
+class MainContainer extends Component{
+     render(){
+        const {isDark,changeTheme} = this.props
+        return(
+            <div className= 'main-container'>
+                <Header theme = {isDark} changeTheme = {changeTheme}/>
+                <div className='profile-container'>
+                    <Footer theme = {isDark} />
+                    <div className='person-profile'>
+                        <div className='details-container'>
+                            <h1 className='greet-text'>Hello</h1>
+                            <h1>I'm <span className='person-name'>Swapnith</span>👋</h1>
+                            <p className='person-info'>UI/UX Designer, Frontend-Developer & Thinker.</p>
+                            <p className='person-info'>Based in India.</p>
+                            <div>
+                                <button className='download-button button'>Download CV</button>
+                                <button className='get-in-touch-button button'>Get in Touch!</button>
+                            </div>
+                        </div>
+                        <div>
+                            <img src = '/person_image.png' className='person-image' alt = "person"/>
+                        </div>
+                    </div>
+                    <div  className='email'>
+                        <p>swapnithmadire123@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        )
+     }
+}
+
+export default MainContainer
